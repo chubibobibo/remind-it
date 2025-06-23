f0988d77f691ec7f977be338ef8a42cbac76c21b
Alistair Abao <abaolester@gmail.com>
2025-06-23 16:11:55 +0200

    added express validation for register inputs

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { register } from "../controllers/userController";
+import { registerValidation } from "../utils/expressValidator/formInputValidation";
 
 const router = express.Router();
 
-router.post("/register", register);
+router.post("/register", registerValidation, register);
 
 export default router;
