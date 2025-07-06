87be5bd92043cfb49793c1d42f610023f7d8a724
Alistair Abao <abaolester@gmail.com>
2025-07-06 21:19:59 +0200

    landing page inital layout with logo

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,19 @@
 function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <>
+      <section className='bg-blue-200 flex'>
+        <section className='w-screen h-screen flex justify-center items-center flex-col transform -translate-y-20'>
+          <img
+            src='../src/assets/note-logo.png'
+            alt=''
+            className='landing-logo-mobile sm: landing-logo'
+          />
+          <section className='landing-card-mobile md: landing-card'>
+            hello
+          </section>
+        </section>
+      </section>
+    </>
+  );
 }
 export default LandingPage;
--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as HomeLayout } from "../pages/HomeLayout";
 export { default as LoginPage } from "../pages/LoginPage";
 export { default as LandingPage } from "../pages/LandingPage";
+export { default as RegisterPage } from "../pages/RegisterPage";
